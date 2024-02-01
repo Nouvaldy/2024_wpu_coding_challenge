b2fc4c04ee3dcb14158ba908e2f1f9ebdf3f2cb6
Aldy <naufaldybambang.w@gmail.com>
2024-02-01 08:39:55 +0700

    day 5

---
+++ 5-zeroFuel.js
@@ -0,0 +1,11 @@
+//WPU Programming Challenge
+//Day 5/366
+//https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return distanceToPump/mpg <= fuelLeft
+//   };
+
+zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump/mpg <= fuelLeft
+
+  console.log(zeroFuel(10, 25, 2))
