8a32c07eec0511e698a14ba2098e3c60745c93da
Aldy <naufaldybambang.w@gmail.com>
2024-02-01 09:41:12 +0700

    day 6

---
+++ 6-DNAtoRNA.js
@@ -0,0 +1,19 @@
+//WPU Programming Challenge
+//Day 6/366
+//https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+
+// function DNAtoRNA(dna) {
+// result=""
+// for(i=0; i<dna.length; i++){
+//     if(dna[i]=="T"){
+//         result += "U"
+//     }
+//     else 
+//     result += dna[i]
+// }
+//     return result
+// }
+
+DNAtoRNA = (dna) => dna.replaceAll("T", "U")
+
+  console.log(DNAtoRNA("GTCTAT"))
