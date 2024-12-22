aa72cf7017d28f64deff7fb1e355aa1545ed2d60
Aldy <naufaldybambang.w@gmail.com>
2024-12-22 11:50:26 +0700

    day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,7 @@
+//WPU Programming Challenge
+//Day 9/366 kita pastikan
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/
+
+function stringToArray(string){
+    return string.split(' ');
+  }
