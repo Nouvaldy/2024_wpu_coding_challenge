3023df8d57bec95ac8b0183b71d6634d7098fafe
Aldy <naufaldybambang.w@gmail.com>
2025-01-06 16:31:30 +0700

    day 39-41/366

--- 39-findOdd.js
+++ 39-findOdd.js
@@ -10,7 +10,7 @@
 //     return parseInt(Object.entries(occurence).filter(([key, values]) => values === -1)[0][0])
 // }
 
-const findOdd = (xs) => xs.reduce((acc, cur) => acc ^ cur); // ^ artinya XOR atau hetero
+const findOdd = (xs) => xs.reduce((acc, cur) => acc ^ cur); // ^ artinya bitwise XOR 
 
 console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])); //5
 
