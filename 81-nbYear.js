d38f980c851fec214740df253554f66ed520bb05
Aldy <naufaldybambang.w@gmail.com>
2025-02-20 15:20:58 +0700

    day 81/366

--- 81-nbYear.js
+++ 81-nbYear.js
@@ -6,10 +6,8 @@ function nbYear(p0, percent, aug, p) {
     for(i=0; ans<p; i++) {
         ans = Math.floor(ans*(100+percent)/100 + aug) ;
     } 
-    return `Pada awal tahun populasi ${p0} jiwa,
-    dengan pertumbuhan ${percent}%/tahun dan ${aug} imigran per tahun,
-    maka dalam ${i} tahun ke depan populasi akan melampaui ${p} bahkan mencapai ${ans} jiwa`;
+    return `Pada awal tahun populasi ${p0} jiwa, dengan pertumbuhan ${percent}%/tahun dan ${aug} imigran per tahun, maka populasi akan melampaui ${p} jiwa dalam waktu ${i} tahun ke depan`;
 }
 
-console.log(nbYear(1500, 5, 100, 5000)); //15
-console.log(nbYear(1000, 2.0, 50, 1214)); //4
+console.log(nbYear(1500, 5, 100, 5000)); //15 tahun
+console.log(nbYear(1000, 2.0, 50, 1214)); //4 tahun
